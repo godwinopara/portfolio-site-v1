b90e062dc25494b1c3b95f6e1742394cb43690f5
Godwin Opara <godwinopara62@yahoo.com>
2021-04-26 19:07:58 +0400

    added html markup and set up sass

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLink = document.querySelector("nav");
+
+hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
+
+function toggleHamburgerMenu() {
+	hamburgerMenu.classList.toggle("show");
+	navLink.classList.toggle("show");
+}
