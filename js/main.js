b4483bd5fdb7633f729964a6af9005b482a79b7f
Godwin Opara <godwinopara62@yahoo.com>
2021-06-30 21:58:56 +0400

    finished the nav menu for mobile layout

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@ hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
 function toggleHamburgerMenu() {
 	hamburgerMenu.classList.toggle("show");
 	navLink.classList.toggle("show-nav");
+	if (navLink.classList.contains("show-nav")) {
+		document.documentElement.style.overflow = "hidden";
+	} else {
+		document.documentElement.style.overflowY = "scroll";
+	}
 }
