6d5b164e831fd675f41b1f807c866b4e7603957c
Godwin Opara <godwinopara62@yahoo.com>
2021-07-03 19:08:52 +0400

    add html content for the about page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLink = document.querySelector("nav");
+const navContact = document.querySelector("#contact");
 const workTogetherContactBtn = document.querySelector("#work-together-contact-btn");
-const contact = document.querySelector(".contact");
+const contact = document.querySelector(".contact-description");
+const hamburgerContainer = document.querySelector(".hamburger-menu");
+const closeIcon = document.querySelector(".close-icon");
+const hamburgerClose = document.querySelector(".hamburger-close");
+const hamburgerOpen = document.querySelector(".hamburger");
 
-hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
+hamburgerOpen.addEventListener("click", toggleHamburgerMenu);
 
+hamburgerClose.addEventListener("click", () => {
+	hamburgerContainer.classList.remove("show");
+	navLink.classList.remove("show-nav");
+	lockScroll(navLink, "show-nav");
+});
 function toggleHamburgerMenu() {
-	hamburgerMenu.classList.toggle("show");
-	navLink.classList.toggle("show-nav");
-	if (navLink.classList.contains("show-nav")) {
+	hamburgerContainer.classList.add("show");
+	navLink.classList.add("show-nav");
+	lockScroll(navLink, "show-nav");
+}
+
+closeIcon.addEventListener("click", () => {
+	contact.classList.remove("show-contact");
+	navLink.classList.remove("show-nav");
+	lockScroll(contact, "show-contact");
+});
+
+navContact.addEventListener("click", showContact);
+
+workTogetherContactBtn.addEventListener("click", showContact);
+
+function lockScroll(itemDisplayed, classname) {
+	if (itemDisplayed.classList.contains(classname)) {
 		document.documentElement.style.overflow = "hidden";
 	} else {
 		document.documentElement.style.overflowY = "scroll";
 	}
 }
 
-workTogetherContactBtn.addEventListener("click", () => {
-	contact.classList.add("active");
-});
+function showContact() {
+	contact.classList.add("show-contact");
+	lockScroll(contact, "show-contact");
+	if (navLink.classList.contains("show-nav")) {
+		navLink.classList.remove("show-nav");
+		hamburgerContainer.classList.remove("show");
+	}
+}
